16a81e8aedd7c28228930565609210583ca7f713
First Last <salmanashraf853@gmail.com>
2019-08-04 21:16:35 +0100

    building the card section

--- src/components/tours/Tours.js
+++ src/components/tours/Tours.js
@@ -8,25 +8,52 @@ const Tours = () => {
             <Heading text={'MOST POPULAR TOURS'}/>
             <div className="cont">
                 <div className="cards">
-                    <div className="card">
+                    <div className="card card1">
                         <div className="side front">
-                            front
+                            <div className="head">
+                                head
+                            </div>
+                            <h2>THE SEA EXPLORER</h2>
+                            <ul>
+                                <li>3 days tour</li>
+                                <li>up to 30 people</li>
+                                <li>2 tour guides</li>
+                                <li>sleep in cozy hotels</li>
+                            </ul>
                         </div>
                         <div className="side back">
                             back
                         </div>
                     </div>
-                    <div className="card">
+                    <div className="card card2">
                         <div className="side front">
-                            front
+                            <div className="head">
+                                head
+                            </div>
+                            <h2>THE SEA EXPLORER</h2>
+                            <ul>
+                                <li>3 days tour</li>
+                                <li>up to 30 people</li>
+                                <li>2 tour guides</li>
+                                <li>sleep in cozy hotels</li>
+                            </ul>
                         </div>
                         <div className="side back">
                             back
                         </div>
                     </div>
-                    <div className="card">
+                    <div className="card card3">
                         <div className="side front">
-                            front
+                            <div className="head">
+                                head
+                            </div>
+                            <h2>THE SEA EXPLORER</h2>
+                            <ul>
+                                <li>3 days tour</li>
+                                <li>up to 30 people</li>
+                                <li>2 tour guides</li>
+                                <li>sleep in cozy hotels</li>
+                            </ul>
                         </div>
                         <div className="side back">
                             back
