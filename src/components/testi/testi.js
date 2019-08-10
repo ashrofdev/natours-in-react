9437b0214461385d631409c1d61e1a9272c5d4cb
First Last <salmanashraf853@gmail.com>
2019-08-10 11:25:26 +0100

    inserting content

--- src/components/testi/testi.js
+++ src/components/testi/testi.js
@@ -10,10 +10,37 @@ const Testi = () => {
             <div className="cont">
                 <div className="cards">
                     <div className="card">
-                    card 1
+                        <div className="conte">
+                            <div className="img">
+                                <img src={require('../img/nat-8.jpg')} />
+                            </div>
+                            <div className="text">
+                                <h4>I HAD THE BEST WEEK EVER WITH MY FAMILY</h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing
+                                     elit. Aperiam, ipsum sapiente aspernatur libero 
+                                     repellat quis consequatur ducimus quam nisi exerci
+                                     tationem omnis earum qui. Aperiam, ipsum sapiente 
+                                     aspernatur libero
+                                </p>
+                            </div>
+                        </div>
                     </div>
                     <div className="card">
-                    card 2
+                        <div className="conte">
+                            <div className="img">
+                                <img src={require('../img/nat-9.jpg')} />
+                            </div>
+                            <div className="text">
+                                <h4>I HAD THE BEST WEEK EVER WITH MY FAMILY</h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                     Aperiam, ipsum sapiente aspernatur libero repellat quis cons
+                                     equatur ducimus quam nisi exercitationem omnis earum qui. Aper
+                                     iam, ipsum sapiente aspernatur libero
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
