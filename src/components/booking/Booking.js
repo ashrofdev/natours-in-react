3e96e4dd9d882c2bd82618563f46f4867e709901
First Last <salmanashraf853@gmail.com>
2019-08-12 14:40:36 +0100

    aligning the contents

--- src/components/booking/Booking.js
+++ src/components/booking/Booking.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import {BtnGreen, Heading} from '../Btns/Btn'
 import './booking.css'
 
 const Booking = () => {
     return (
         <div className="booking">
-            <div className="cont"></div>
+            <div className="cont">
+                <div className="book">
+                    <Heading text="START BOOKING NOW" />
+                    <input placeholder="Full Name"/>
+                    <input placeholder="Email Address" type="email"/>
+                    <div className="checks">
+                        <input placeholder="testing" type="radio"/>
+                        <input type="radio"/>
+                    </div>
+                    <BtnGreen name="NEXT STEP"/>
+                </div>
+            </div>
         </div>
     )
 }
