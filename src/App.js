6293a4957df6a9cc2e99b47b450d71c2de47b852
First Last <salmanashraf853@gmail.com>
2019-09-06 11:30:31 +0100

    macking natours responsive n tab view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header/Header'
 import './App.css';
-import {BtnWhite, BtnGreen} from './components/Btns/Btn'
 import Story from './components/Story/Story';
 import Features from './components/features/Features';
 import Tours from './components/tours/Tours';
--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './features.css'
+import 'font-awesome/css/font-awesome.min.css'
 
 const Features = () => {
     return (
         <div className="features">
             <div className="cont">
                 <div className="feature">
-                    <div className="icon">icon</div>
+                    <div className="icon">
+                        <i className="fa fa-globe"></i>
+                    </div>
                     <h3>EXPLORE THE WORLD</h3>
                     <p>
                         Lorem ipsum dolor sit amet consectetur 
@@ -14,24 +17,30 @@ const Features = () => {
                     </p>
                 </div>
                 <div className="feature">
-                    <div className="icon">icon</div>
-                    <h3>EXPLORE THE WORLD</h3>
+                    <div className="icon">
+                        <i className="fa fa-compass"></i>
+                    </div>
+                    <h3>MEET NATURE</h3>
                     <p>
                         Lorem ipsum dolor sit amet consectetur 
                         adipisicing elit. Aperiam, ipsum aspernatur.
                     </p>
                 </div>
                 <div className="feature">
-                    <div className="icon">icon</div>
-                    <h3>EXPLORE THE WORLD</h3>
+                    <div className="icon">
+                        <i className="fa fa-map"></i>
+                    </div>
+                    <h3>FIND YOUR WAY</h3>
                     <p>
                         Lorem ipsum dolor sit amet consectetur 
                         adipisicing elit. Aperiam, ipsum aspernatur.
                     </p>
                 </div>
                 <div className="feature">
-                    <div className="icon">icon</div>
-                    <h3>EXPLORE THE WORLD</h3>
+                    <div className="icon">
+                        <i className="fa fa-heart"></i>
+                    </div>
+                    <h3>LIVE A HEALTHIER LIFE</h3>
                     <p>
                         Lorem ipsum dolor sit amet consectetur 
                         adipisicing elit. Aperiam, ipsum aspernatur.
