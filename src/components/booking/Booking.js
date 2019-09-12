28e4407ef122da9cf04f9002ca82a0bbbc7e24b4
First Last <salmanashraf853@gmail.com>
2019-09-12 16:00:12 +0100

    styling booking checkbox

--- src/components/booking/Booking.js
+++ src/components/booking/Booking.js
@@ -2,6 +2,15 @@ import React from 'react'
 import {BtnGreen, Heading} from '../Btns/Btn'
 import './booking.css'
 
+const onclick = () => {
+    document.querySelector('.r1').classList.toggle('check')
+    document.querySelector('.r2').classList.remove('check')
+}
+const onclick2 = () => {
+    document.querySelector('.r2').classList.toggle('check')
+    document.querySelector('.r1').classList.remove('check')
+}
+
 const Booking = () => {
     return (
         <div className="booking">
@@ -12,10 +21,16 @@ const Booking = () => {
                     <input className="input" placeholder="Email Address" type="email"/>
                     <div className="checks">
                         <label>
-                            <input name="tour" placeholder="testing" type="radio"/>Small tour group
+                            <div className="r r1">
+                                
+                            </div>
+                            <input onChange={onclick} name="tour" placeholder="testing" type="radio"/>Small tour group
                         </label>
                         <label>
-                            <input name="tour" placeholder="testing" type="radio"/>Large tour group
+                            <div className="r r2">
+                                
+                            </div>
+                            <input onChange={onclick2} name="tour" placeholder="testing" type="radio"/>Large tour group
                         </label>
                     </div>
                     <BtnGreen name="NEXT STEP"/>
