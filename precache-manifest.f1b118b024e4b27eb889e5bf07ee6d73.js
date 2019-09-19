22341615c1df5f751bfb5c9d75e3ccade8c76747
First Last <salmanashraf853@gmail.com>
2019-09-19 16:22:08 +0100

    Updates

---
+++ precache-manifest.f1b118b024e4b27eb889e5bf07ee6d73.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98285f59ca8aa90cc2fe12b1871f26d6",
+    "url": "/natours-in-react/index.html"
+  },
+  {
+    "revision": "c888d0e448f1fae8ae6d",
+    "url": "/natours-in-react/static/css/2.2efec9e8.chunk.css"
+  },
+  {
+    "revision": "5d6d089d846ebc5e6ae4",
+    "url": "/natours-in-react/static/css/main.f2fae9a9.chunk.css"
+  },
+  {
+    "revision": "c888d0e448f1fae8ae6d",
+    "url": "/natours-in-react/static/js/2.e3edc809.chunk.js"
+  },
+  {
+    "revision": "5d6d089d846ebc5e6ae4",
+    "url": "/natours-in-react/static/js/main.81e916c8.chunk.js"
+  },
+  {
+    "revision": "06dd57bfb56bc1398ddc",
+    "url": "/natours-in-react/static/js/runtime~main.d5aade79.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/natours-in-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/natours-in-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/natours-in-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/natours-in-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/natours-in-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6e09f4102b0d64adc38d21bd9c89a9a7",
+    "url": "/natours-in-react/static/media/hero.6e09f410.jpg"
+  },
+  {
+    "revision": "4c42c11f1fc0203483c21f2d6eeb8896",
+    "url": "/natours-in-react/static/media/logo-green-1x.4c42c11f.png"
+  },
+  {
+    "revision": "d6c15bf0efcaf4c03c5cb0c9078cdd17",
+    "url": "/natours-in-react/static/media/nat-1.d6c15bf0.jpg"
+  },
+  {
+    "revision": "2797561ff2a3de15c850f2744b73b6d1",
+    "url": "/natours-in-react/static/media/nat-10.2797561f.jpg"
+  },
+  {
+    "revision": "4d3783688636b59662283e19991a2912",
+    "url": "/natours-in-react/static/media/nat-2.4d378368.jpg"
+  },
+  {
+    "revision": "cc2b05433cbad0b369d131d02c0cac6a",
+    "url": "/natours-in-react/static/media/nat-3.cc2b0543.jpg"
+  },
+  {
+    "revision": "19f9a22f45a15fbf80d420862bcadffb",
+    "url": "/natours-in-react/static/media/nat-5.19f9a22f.jpg"
+  },
+  {
+    "revision": "1ef6d764fc819e080d40aa72dfe03aa6",
+    "url": "/natours-in-react/static/media/nat-6.1ef6d764.jpg"
+  },
+  {
+    "revision": "5f846d16e6be9a73e6eb73f620379af2",
+    "url": "/natours-in-react/static/media/nat-7.5f846d16.jpg"
+  },
+  {
+    "revision": "14efd82b81787a9764b2cb7eb7afd3e8",
+    "url": "/natours-in-react/static/media/nat-8.14efd82b.jpg"
+  },
+  {
+    "revision": "b525a99bcf20d08b2aaf6dd83b7be623",
+    "url": "/natours-in-react/static/media/nat-9.b525a99b.jpg"
+  },
+  {
+    "revision": "ffcd876c5561db066c09f5922b384473",
+    "url": "/natours-in-react/static/media/video.ffcd876c.mp4"
+  }
+]);
